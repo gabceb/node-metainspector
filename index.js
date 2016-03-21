2524c5a7827f45bad75e7c91c352b83d0b130c7f
Feldmann, Tom <tfeldmann@follett.com>
2016-03-21 14:38:14 -0500

    add request headers
    Added the ability to set your own headers on the request.
    Added a default User-Agent header to fix an issue where some websites
    would return a 403 status without it.
    Removed deprecated __proto__ assignment in favor of setPrototypeOf
    function.
    Misc cleanup to get the project to build.

--- index.js
+++ index.js
@@ -36,156 +36,140 @@ var MetaInspector = function(url, options){
 	this.timeout = this.options.timeout || 20000; //Timeout in ms
 
 	this.strictSSL = !!this.options.strictSSL;
+
+	//some urls will not work if you do not include a user-agent header
+	//this gives you the flexability to set your headers how you see fit
+	this.headers = this.options.headers ? this.options.headers : {'User-Agent': 'MetaInspector/1.0'};
 };
 
 //MetaInspector.prototype = new events.EventEmitter();
-MetaInspector.prototype.__proto__ = events.EventEmitter.prototype;
+Object.setPrototypeOf(MetaInspector.prototype, events.EventEmitter.prototype);
 
 module.exports = MetaInspector;
 
-MetaInspector.prototype.getTitle = function()
-{
+MetaInspector.prototype.getTitle = function() {
 	debug("Parsing page title");
 
-	if(!this.title)
-	{
+	if (!this.title) {
 		this.title = this.parsedDocument('head > title').text();
 	}
 
 	return this;
-}
+};
 
-MetaInspector.prototype.getOgTitle = function()
-{
+MetaInspector.prototype.getOgTitle = function() {
 	debug("Parsing page Open Graph title");
 
-	if(!this.ogTitle)
-	{
+	if (!this.ogTitle) {
 		this.ogTitle = this.parsedDocument("meta[property='og:title']").attr("content");
 	}
 
 	return this;
-}
+};
 
-MetaInspector.prototype.getOgDescription = function()
-{
+MetaInspector.prototype.getOgDescription = function() {
 	debug("Parsing page Open Graph description");
 
-	if(this.ogDescription === undefined)
-	{
+	if (this.ogDescription === undefined) {
 		this.ogDescription = this.parsedDocument("meta[property='og:description']").attr("content");
 	}
 
 	return this;
-}
+};
 
-MetaInspector.prototype.getOgType = function()
-{
+MetaInspector.prototype.getOgType = function() {
 	debug("Parsing page's Open Graph Type");
 
-	if(this.ogType === undefined)
-	{
+	if (this.ogType === undefined) {
 		this.ogType = this.parsedDocument("meta[property='og:type']").attr("content");
 	}
 
 	return this;
-}
+};
 
-MetaInspector.prototype.getOgUpdatedTime = function()
-{
+MetaInspector.prototype.getOgUpdatedTime = function() {
 	debug("Parsing page's Open Graph Updated Time");
 
-	if(this.ogUpdatedTime === undefined)
-	{
+	if (this.ogUpdatedTime === undefined) {
 		this.ogUpdatedTime = this.parsedDocument("meta[property='og:updated_time']").attr("content");
 
 		return this;
 	}
-}
+};
 
-MetaInspector.prototype.getOgLocale = function()
-{
+MetaInspector.prototype.getOgLocale = function() {
 	debug("Parsing page's Open Graph Locale");
 
-	if(this.ogLocale === undefined)
-	{
+	if (this.ogLocale === undefined) {
 		this.ogLocale = this.parsedDocument("meta[property='og:locale']").attr("content");
 	}
 
 	return this;
-}
+};
 
-MetaInspector.prototype.getLinks = function()
-{
+MetaInspector.prototype.getLinks = function() {
 	debug("Parsing page links");
 
 	var _this = this;
 
-	if(!this.links)
-	{
-		this.links = this.parsedDocument('a').map(function(i ,elem){
+	if (!this.links) {
+		this.links = this.parsedDocument('a').map(function(i ,elem) {
 			return _this.parsedDocument(this).attr('href');
 		});
 	}
 
 	return this;
-}
+};
 
-MetaInspector.prototype.getMetaDescription = function()
-{
+MetaInspector.prototype.getMetaDescription = function() {
 	debug("Parsing page description based on meta elements");
 
-	if(!this.description)
-	{
+	if (!this.description) {
 		this.description = this.parsedDocument("meta[name='description']").attr("content");
 	}
 
 	return this;
-}
+};
 
-MetaInspector.prototype.getSecondaryDescription = function()
-{
+MetaInspector.prototype.getSecondaryDescription = function() {
 	debug("Parsing page secondary description");
 	var _this = this;
 
-	if(!this.description)
-	{
+	if (!this.description) {
 		var minimumPLength = 120;
 
 		this.parsedDocument("p").each(function(i, elem){
-			if(_this.description){
+			if (_this.description){
 				return;
 			}
 
 			var text = _this.parsedDocument(this).text();
 
 			// If we found a paragraph with more than
-			if(text.length >= minimumPLength) {
+			if (text.length >= minimumPLength) {
 				_this.description = text;
 			}
 		});
 	}
 
 	return this;
-}
+};
 
-MetaInspector.prototype.getDescription = function()
-{
+MetaInspector.prototype.getDescription = function() {
 	debug("Parsing page description based on meta description or secondary description");
-	this.getMetaDescription() && this.getSecondaryDescription();
+	this.getMetaDescription();
+	this.getSecondaryDescription();
 
 	return this;
-}
+};
 
-MetaInspector.prototype.getKeywords = function()
-{
+MetaInspector.prototype.getKeywords = function() {
 	debug("Parsing page keywords from apropriate metatag");
 
-	if(!this.keywords)
-	{
+	if (!this.keywords) {
 		var keywordsString = this.parsedDocument("meta[name='keywords']").attr("content");
 
-		if(keywordsString) {
+		if (keywordsString) {
 			this.keywords = keywordsString.split(',');
 		} else {
 			this.keywords = [];
@@ -193,87 +177,75 @@ MetaInspector.prototype.getKeywords = function()
 	}
 
 	return this;
-}
+};
 
-MetaInspector.prototype.getAuthor = function()
-{
+MetaInspector.prototype.getAuthor = function() {
 	debug("Parsing page author from apropriate metatag");
 
-	if(!this.author)
-	{
+	if (!this.author) {
 		this.author = this.parsedDocument("meta[name='author']").attr("content");
 	}
 
 	return this;
-}
+};
 
-MetaInspector.prototype.getCharset = function()
-{
+MetaInspector.prototype.getCharset = function() {
 	debug("Parsing page charset from apropriate metatag");
 
-	if(!this.charset)
-	{
+	if (!this.charset) {
 		this.charset = this.parsedDocument("meta[charset]").attr("charset");
 	}
 
 	return this;
-}
+};
 
-MetaInspector.prototype.getImage = function()
-{
+MetaInspector.prototype.getImage = function() {
 	debug("Parsing page image based on the Open Graph image");
 
-	if(!this.image)
-	{
+	if (!this.image) {
 		var img = this.parsedDocument("meta[property='og:image']").attr("content");
-		if (img){
+		if (img) {
 			this.image = this.getAbsolutePath(img);
 		}
 	}
 
 	return this;
-}
+};
 
-MetaInspector.prototype.getImages = function()
-{
+MetaInspector.prototype.getImages = function() {
 	debug("Parsing page body images");
 	var _this = this;
 
-	if(this.images === undefined)
-	{
-		this.images = this.parsedDocument('img').map(function(i ,elem){
+	if (this.images === undefined) {
+		this.images = this.parsedDocument('img').map(function(i ,elem) {
 			var src = _this.parsedDocument(this).attr('src');
 			return _this.getAbsolutePath(src);
 		});
 	}
 
 	return this;
-}
+};
 
-MetaInspector.prototype.getFeeds = function()
-{
+MetaInspector.prototype.getFeeds = function() {
 	debug("Parsing page feeds based on rss or atom feeds");
 
-	if(!this.feeds)
-	{
+	if (!this.feeds) {
 		this.feeds = this.parseFeeds("rss") || this.parseFeeds("atom");
 	}
 
 	return this;
-}
+};
 
-MetaInspector.prototype.parseFeeds = function(format)
-{
+MetaInspector.prototype.parseFeeds = function(format) {
 	var _this = this;
-	var feeds = this.parsedDocument("link[type='application/" + format + "+xml']").map(function(i ,elem){
+	var feeds = this.parsedDocument("link[type='application/" + format + "+xml']").map(function(i ,elem) {
 		return _this.parsedDocument(this).attr('href');
 	});
 
 	return feeds;
-}
+};
 
-MetaInspector.prototype.initAllProperties = function()
-{
+MetaInspector.prototype.initAllProperties = function() {
 	// title of the page, as string
 	this.getTitle()
 			.getAuthor()
@@ -289,20 +261,20 @@ MetaInspector.prototype.initAllProperties = function()
 			.getOgType()
 			.getOgUpdatedTime()
 			.getOgLocale();
-}
+};
 
-MetaInspector.prototype.getAbsolutePath = function(href){
-	if((/^(http:|https:)?\/\//i).test(href)) { return href; }
-	if(!(/^\//).test(href)){ href = '/' + href; }
+MetaInspector.prototype.getAbsolutePath = function(href) {
+	if ((/^(http:|https:)?\/\//i).test(href)) { return href; }
+	if (!(/^\//).test(href)){ href = '/' + href; }
 	return this.rootUrl + href;
 };
 
-MetaInspector.prototype.fetch = function(){
+MetaInspector.prototype.fetch = function() {
 	var _this = this;
 	var totalChunks = 0;
 
-	var r = request({uri : this.url, gzip: true, maxRedirects: this.maxRedirects, timeout: this.timeout, strictSSL: this.strictSSL}, function(error, response, body){
-		if(!error && response.statusCode === 200){
+	var r = request({uri : this.url, gzip: true, maxRedirects: this.maxRedirects, timeout: this.timeout, strictSSL: this.strictSSL, headers: this.headers}, function(error, response, body){
+		if (!error && response.statusCode === 200){
 			_this.document = body;
 			_this.parsedDocument = cheerio.load(body);
 			_this.response = response;
@@ -316,12 +288,12 @@ MetaInspector.prototype.fetch = function(){
 		}
 	});
 
-	if(_this.options.limit){
+	if (_this.options.limit) {
 		_this.__stoppedAtLimit = false;
 		r.on('data', function(chunk){
 			totalChunks += chunk.length;
-			if(totalChunks > _this.options.limit){
-				if(!_this.__stoppedAtLimit) {
+			if (totalChunks > _this.options.limit){
+				if (!_this.__stoppedAtLimit) {
 					_this.emit("limit");
 					_this.__stoppedAtLimit = true;
 				}
