fa2c6eb1c1bbc6ba40d41a81e12e67f447d1a3c8
Sjoerd Stottelaar <stottelaar.s@gmail.com>
2018-01-28 09:10:05 +0100

    Added a property to check if a meta description is present or not.

--- index.js
+++ index.js
@@ -141,6 +141,7 @@ MetaInspector.prototype.getMetaDescription = function()
 	if(!this.description)
 	{
 		this.description = this.parsedDocument("meta[name='description']").attr("content");
+		this.descriptionPresent = true;
 	}
 
 	return this;
@@ -154,6 +155,7 @@ MetaInspector.prototype.getSecondaryDescription = function()
 	if(!this.description)
 	{
 		var minimumPLength = 120;
+		this.descriptionPresent = false;
 
 		this.parsedDocument("p").each(function(i, elem){
 			if(_this.description){
