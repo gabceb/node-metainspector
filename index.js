7e3b83148ce7aabab0979b7f52fd133fe8451ff4
Gabriel Cebrian <gabceb@gmail.com>
2019-02-16 09:08:25 -0500

    Code changes for readability and linting

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var util = require('util'),
-	request = require('request'),
-	events = require('events'),
-	cheerio = require('cheerio'),
-	URI = require('uri-js'),
-	_ = require('lodash');
+var util = require('util');
+var request = require('request');
+var events = require('events');
+var cheerio = require('cheerio');
+var URI = require('uri-js');
+var _ = require('lodash');
 
 var debug;
 
@@ -15,11 +15,11 @@ if (/\bmetainspector\b/.test(process.env.NODE_DEBUG)) {
   debug = function() {};
 }
 
-function withDefaultScheme(url){
+function withDefaultScheme(url) {
 	return URI.parse(url).scheme ? url : "http://" + url;
 }
 
-var MetaInspector = function(url, options){
+var MetaInspector = function(url, options) {
 	this.url = URI.normalize(withDefaultScheme(url));
 
 	this.parsedUrl = URI.parse(this.url);
@@ -38,16 +38,16 @@ var MetaInspector = function(url, options){
 
 	this.options.strictSSL = !!this.options.strictSSL;
 
-	this.options.headers = this.options.headers || {'User-Agent' : 'MetaInspector/1.0'};
+	this.options.headers = this.options.headers || { 'User-Agent' : 'MetaInspector/1.0' };
 };
 
 //MetaInspector.prototype = new events.EventEmitter();
-MetaInspector.prototype.__proto__ = events.EventEmitter.prototype;
+// MetaInspector.prototype.__proto__ = events.EventEmitter.prototype;
+util.inherits(MetaInspector, events.EventEmitter);
 
 module.exports = MetaInspector;
 
-MetaInspector.prototype.getTitle = function()
-{
+MetaInspector.prototype.getTitle = function() {
 	debug("Parsing page title");
 
 	if(!this.title)
@@ -56,10 +56,9 @@ MetaInspector.prototype.getTitle = function()
 	}
 
 	return this;
-}
+};
 
-MetaInspector.prototype.getOgTitle = function()
-{
+MetaInspector.prototype.getOgTitle = function() {
 	debug("Parsing page Open Graph title");
 
 	if(!this.ogTitle)
@@ -68,10 +67,9 @@ MetaInspector.prototype.getOgTitle = function()
 	}
 
 	return this;
-}
+};
 
-MetaInspector.prototype.getOgDescription = function()
-{
+MetaInspector.prototype.getOgDescription = function() {
 	debug("Parsing page Open Graph description");
 
 	if(this.ogDescription === undefined)
@@ -80,10 +78,9 @@ MetaInspector.prototype.getOgDescription = function()
 	}
 
 	return this;
-}
+};
 
-MetaInspector.prototype.getOgType = function()
-{
+MetaInspector.prototype.getOgType = function() {
 	debug("Parsing page's Open Graph Type");
 
 	if(this.ogType === undefined)
@@ -92,7 +89,7 @@ MetaInspector.prototype.getOgType = function()
 	}
 
 	return this;
-}
+};
 
 MetaInspector.prototype.getOgUpdatedTime = function()
 {
@@ -104,10 +101,9 @@ MetaInspector.prototype.getOgUpdatedTime = function()
 
 		return this;
 	}
-}
+};
 
-MetaInspector.prototype.getOgLocale = function()
-{
+MetaInspector.prototype.getOgLocale = function() {
 	debug("Parsing page's Open Graph Locale");
 
 	if(this.ogLocale === undefined)
@@ -116,26 +112,24 @@ MetaInspector.prototype.getOgLocale = function()
 	}
 
 	return this;
-}
+};
 
-MetaInspector.prototype.getLinks = function()
-{
+MetaInspector.prototype.getLinks = function() {
 	debug("Parsing page links");
 
 	var _this = this;
 
 	if(!this.links)
 	{
-		this.links = this.parsedDocument('a').map(function(i ,elem){
+		this.links = this.parsedDocument('a').map(function(i ,elem) {
 			return _this.parsedDocument(this).attr('href');
 		});
 	}
 
 	return this;
-}
+};
 
-MetaInspector.prototype.getMetaDescription = function()
-{
+MetaInspector.prototype.getMetaDescription = function() {
 	debug("Parsing page description based on meta elements");
 
 	if(!this.description)
@@ -144,19 +138,17 @@ MetaInspector.prototype.getMetaDescription = function()
 	}
 
 	return this;
-}
+};
 
-MetaInspector.prototype.getSecondaryDescription = function()
-{
+MetaInspector.prototype.getSecondaryDescription = function() {
 	debug("Parsing page secondary description");
 	var _this = this;
 
-	if(!this.description)
-	{
+	if(!this.description) {
 		var minimumPLength = 120;
 
-		this.parsedDocument("p").each(function(i, elem){
-			if(_this.description){
+		this.parsedDocument("p").each(function(i, elem) {
+			if(_this.description) {
 				return;
 			}
 
@@ -170,22 +162,20 @@ MetaInspector.prototype.getSecondaryDescription = function()
 	}
 
 	return this;
-}
+};
 
-MetaInspector.prototype.getDescription = function()
-{
+MetaInspector.prototype.getDescription = function() {
 	debug("Parsing page description based on meta description or secondary description");
-	this.getMetaDescription() && this.getSecondaryDescription();
+	this.getMetaDescription();
+	this.getSecondaryDescription();
 
 	return this;
-}
+};
 
-MetaInspector.prototype.getKeywords = function()
-{
+MetaInspector.prototype.getKeywords = function() {
 	debug("Parsing page keywords from apropriate metatag");
 
-	if(!this.keywords)
-	{
+	if(!this.keywords) {
 		var keywordsString = this.parsedDocument("meta[name='keywords']").attr("content");
 
 		if(keywordsString) {
@@ -196,87 +186,75 @@ MetaInspector.prototype.getKeywords = function()
 	}
 
 	return this;
-}
+};
 
-MetaInspector.prototype.getAuthor = function()
-{
+MetaInspector.prototype.getAuthor = function() {
 	debug("Parsing page author from apropriate metatag");
 
-	if(!this.author)
-	{
+	if(!this.author) {
 		this.author = this.parsedDocument("meta[name='author']").attr("content");
 	}
 
 	return this;
-}
+};
 
-MetaInspector.prototype.getCharset = function()
-{
+MetaInspector.prototype.getCharset = function() {
 	debug("Parsing page charset from apropriate metatag");
 
-	if(!this.charset)
-	{
+	if(!this.charset) {
 		this.charset = this.parsedDocument("meta[charset]").attr("charset");
 	}
 
 	return this;
-}
+};
 
-MetaInspector.prototype.getImage = function()
-{
+MetaInspector.prototype.getImage = function() {
 	debug("Parsing page image based on the Open Graph image");
 
-	if(!this.image)
-	{
+	if(!this.image) {
 		var img = this.parsedDocument("meta[property='og:image']").attr("content");
-		if (img){
+		if (img) {
 			this.image = this.getAbsolutePath(img);
 		}
 	}
 
 	return this;
-}
+};
 
-MetaInspector.prototype.getImages = function()
-{
+MetaInspector.prototype.getImages = function() {
 	debug("Parsing page body images");
 	var _this = this;
 
-	if(this.images === undefined)
-	{
-		this.images = this.parsedDocument('img').map(function(i ,elem){
+	if(this.images === undefined) {
+		this.images = this.parsedDocument('img').map(function(i ,elem) {
 			var src = _this.parsedDocument(this).attr('src');
 			return _this.getAbsolutePath(src);
 		});
 	}
 
 	return this;
-}
+};
 
-MetaInspector.prototype.getFeeds = function()
-{
+MetaInspector.prototype.getFeeds = function() {
 	debug("Parsing page feeds based on rss or atom feeds");
 
-	if(!this.feeds)
-	{
+	if(!this.feeds) {
 		this.feeds = this.parseFeeds("rss") || this.parseFeeds("atom");
 	}
 
 	return this;
-}
+};
 
-MetaInspector.prototype.parseFeeds = function(format)
-{
+MetaInspector.prototype.parseFeeds = function(format) {
 	var _this = this;
-	var feeds = this.parsedDocument("link[type='application/" + format + "+xml']").map(function(i ,elem){
+	var feeds = this.parsedDocument("link[type='application/" + format + "+xml']").map(function(i ,elem) {
 		return _this.parsedDocument(this).attr('href');
 	});
 
 	return feeds;
-}
+};
 
-MetaInspector.prototype.initAllProperties = function()
-{
+MetaInspector.prototype.initAllProperties = function() {
 	// title of the page, as string
 	this.getTitle()
 			.getAuthor()
@@ -292,19 +270,22 @@ MetaInspector.prototype.initAllProperties = function()
 			.getOgType()
 			.getOgUpdatedTime()
 			.getOgLocale();
-}
+};
 
-MetaInspector.prototype.getAbsolutePath = function(href){
+MetaInspector.prototype.getAbsolutePath = function(href) {
 	if((/^(http:|https:)?\/\//i).test(href)) { return href; }
-	if(!(/^\//).test(href)){ href = '/' + href; }
+	if(!(/^\//).test(href)) {
+		href = '/' + href;
+	}
+
 	return this.rootUrl + href;
 };
 
-MetaInspector.prototype.fetch = function(){
+MetaInspector.prototype.fetch = function() {
 	var _this = this;
 	var totalChunks = 0;
-	var r = request(_.assign({uri : this.url, gzip: true}, this.options), function(error, response, body){
-		if(!error && response.statusCode === 200){
+	var r = request(_.assign({uri : this.url, gzip: true}, this.options), function(error, response, body) {
+		if(!error && response.statusCode === 200) {
 			_this.document = body;
 			_this.parsedDocument = cheerio.load(body);
 			_this.response = response;
@@ -318,11 +299,11 @@ MetaInspector.prototype.fetch = function(){
 		}
 	});
 
-	if(_this.options.limit){
+	if(_this.options.limit) {
 		_this.__stoppedAtLimit = false;
-		r.on('data', function(chunk){
+		r.on('data', function(chunk) {
 			totalChunks += chunk.length;
-			if(totalChunks > _this.options.limit){
+			if(totalChunks > _this.options.limit) {
 				if(!_this.__stoppedAtLimit) {
 					_this.emit("limit");
 					_this.__stoppedAtLimit = true;
--- test/test.js
+++ test/test.js
@@ -1,20 +1,20 @@
 require('mocha');
 
-var MetaInspector = require('../index'),
-	should = require('should');
+var MetaInspector = require('../index');
+var should = require('should');
 
 require('./fixtures/fixtures');
 
-describe('metainspector', function(){
+describe('metainspector', function() {
 
-	describe('multiple clients', function(){
+	describe('multiple clients', function() {
 		var firstClient = new MetaInspector('http://www.google.com');
 		var secondClient = new MetaInspector('http://www.google.com');
 
-		it('should not keep the same eventEmitter reference among clients', function(done){
+		it('should not keep the same eventEmitter reference among clients', function(done) {
 			var calledOnce = false;
 
-			firstClient.on('fetch', function(){
+			firstClient.on('fetch', function() {
 				if (!calledOnce) {
 					calledOnce = true;
 				} else {
@@ -22,10 +22,12 @@ describe('metainspector', function(){
 				}
 			});
 
-			secondClient.on('fetch', function(){
-				should.exists(secondClient.parsedDocument);
+			secondClient.on('fetch', function() {
+				should.exist(secondClient.parsedDocument);
 
-				if (calledOnce) done();
+				if (calledOnce) {
+					done();
+				}
 			});
 
 			firstClient.fetch();
@@ -33,59 +35,60 @@ describe('metainspector', function(){
 		});
 	});
 
-	describe('client', function(){
+	describe('client', function() {
 		var client = null;
 
-		it('should have a url property', function(done){
+		it('should have a url property', function(done) {
 			client = new MetaInspector("http://www.google.com");
 
 			client.url.should.equal("http://www.google.com/");
 			done();
 		});
 
-		it('should add http as the default scheme if no scheme is passed', function(done){
+		it('should add http as the default scheme if no scheme is passed', function(done) {
 			client = new MetaInspector("www.google.com");
 
 			client.url.should.equal("http://www.google.com/");
 			done();
 		});
 
-		it('should have a scheme property', function(done){
+		it('should have a scheme property', function(done) {
 			client = new MetaInspector("http://www.google.com");
 
 			client.scheme.should.equal("http");
 			done();
 		});
 
-		it('should have a host property', function(done){
+		it('should have a host property', function(done) {
 			client = new MetaInspector("http://www.google.com");
 
 			client.host.should.equal("www.google.com");
 			done();
 		});
 
-		it('should have a rootUrl property', function(done){
+		it('should have a rootUrl property', function(done) {
 			client = new MetaInspector("http://www.google.com");
 
 			client.rootUrl.should.equal("http://www.google.com");
 			done();
 		});
 
-		it('should have a parsedDocument', function(done){
+		it('should have a parsedDocument', function(done) {
 			client = new MetaInspector("http://www.google.com");
 
-			client.once("fetch", function(){
-				should.exists(client.parsedDocument);
+			client.once("fetch", function() {
+				should.exist(client.parsedDocument);
+
 				done();
 			});
 
 			client.fetch();
 		});
 
-		it('should have a title', function(done){
+		it('should have a title', function(done) {
 			client = new MetaInspector("http://www.google.com", {});
 
-			client.once("fetch", function(){
+			client.once("fetch", function() {
 				client.title.should.equal("Google");
 				done();
 			});
@@ -93,10 +96,10 @@ describe('metainspector', function(){
 			client.fetch();
 		});
 
-		it('should have keywords', function(done){
+		it('should have keywords', function(done) {
 			client = new MetaInspector("http://www.simple.com", {});
 
-			client.once("fetch", function(){
+			client.once("fetch", function() {
 				client.keywords.should.be.instanceof(Array).and.be.eql([ 'HTML', 'CSS', 'XML', 'JavaScript' ]).and.have.lengthOf(4);
 				done();
 			});
@@ -104,10 +107,10 @@ describe('metainspector', function(){
 			client.fetch();
 		});
 
-		it('keywords should be undefined if there is no keywords', function(done){
+		it('keywords should be undefined if there is no keywords', function(done) {
 			client = new MetaInspector("http://www.google.com", {});
 
-			client.once("fetch", function(){
+			client.once("fetch", function() {
 				client.keywords.should.be.instanceof(Array).and.be.eql([]);
 				done();
 			});
@@ -115,65 +118,70 @@ describe('metainspector', function(){
 			client.fetch();
 		});
 
-		it('author should be undefined if there is no author', function(done){
+		it('author should be undefined if there is no author', function(done) {
 			client = new MetaInspector("http://www.google.com", {});
 
-			client.once("fetch", function(){
+			client.once("fetch", function() {
 				should.not.exist(client.author);
+
 				done();
 			});
 
 			client.fetch();
 		});
 
-		it('charset should be undefined if there is no charset', function(done){
+		it('charset should be undefined if there is no charset', function(done) {
 			client = new MetaInspector("http://www.google.com", {});
 
-			client.once("fetch", function(){
+			client.once("fetch", function() {
 				should.not.exist(client.charset);
+
 				done();
 			});
 
 			client.fetch();
 		});
 
-		it('should have author', function(done){
+		it('should have author', function(done) {
 			client = new MetaInspector("http://www.simple.com", {});
 
-			client.once("fetch", function(){
+			client.once("fetch", function() {
 				client.author.should.be.equal('Author Name');
+
 				done();
 			});
 
 			client.fetch();
 		});
 
-		it('should have charset', function(done){
+		it('should have charset', function(done) {
 			client = new MetaInspector("http://www.simple.com", {});
 
-			client.once("fetch", function(){
+			client.once("fetch", function() {
 				client.charset.should.be.equal('UTF-8');
+
 				done();
 			});
 
 			client.fetch();
 		});
 
-		it('should have links returned as an array', function(done){
+		it('should have links returned as an array', function(done) {
 			client = new MetaInspector("http://www.google.com", {});
 
-			client.once("fetch", function(){
+			client.once("fetch", function() {
 				client.links.length.should.equal(51);
+
 				done();
 			});
 
 			client.fetch();
 		});
 
-		it('should have a description', function(done){
+		it('should have a description', function(done) {
 			client = new MetaInspector("http://www.google.com", {});
 
-			client.once("fetch", function(){
+			client.once("fetch", function() {
 				client.description.should.equal("Search the world's information, including webpages, images, videos and more. Google has many special features to help you find exactly what you're looking for.");
 				done();
 			});
@@ -181,21 +189,22 @@ describe('metainspector', function(){
 			client.fetch();
 		});
 
-		it('should have a og:image with relative path and return as absolute', function(done){
+		it('should have a og:image with relative path and return as absolute', function(done) {
 			client = new MetaInspector("http://www.fastandfurious7-film.com");
 
-			client.once("fetch", function(){
+			client.once("fetch", function() {
 				client.image.should.equal("http://www.fastandfurious7-film.com/images/fb.jpg");
+
 				done();
 			});
 
 			client.fetch();
 		});
 
-		it('should have a og:description', function(done){
+		it('should have a og:description', function(done) {
 			client = new MetaInspector("http://www.fastandfurious7-film.com");
 
-			client.once("fetch", function(){
+			client.once("fetch", function() {
 				client.ogDescription.should.equal("Continuing the global exploits in the unstoppable franchise built on speed, Vin Diesel, Paul Walker and Dwayne Johnson lead the returning cast of Fast & Furious 7.");
 				done();
 			});
@@ -203,43 +212,46 @@ describe('metainspector', function(){
 			client.fetch();
 		});
 
-		it('should return undefined if the meta description is not defined when metaDescription used', function(done){
+		it('should return undefined if the meta description is not defined when metaDescription used', function(done) {
 			client = new MetaInspector("http://www.simple.com", {});
 
-			client.once("fetch", function(){
+			client.once("fetch", function() {
 				should.not.exist(client.metaDescription);
+
 				done();
 			});
 
 			client.fetch();
 		});
 
-		it('should find a secondary description if there is no description meta element', function(done){
+		it('should find a secondary description if there is no description meta element', function(done) {
 			client = new MetaInspector("http://www.simple.com", {});
 
-			client.once("fetch", function(){
+			client.once("fetch", function() {
 				client.description.should.equal("This is a new paragraph! This paragraph should be very long so we can grab it as the secondary description. What do you think of that?");
+
 				done();
 			});
 
 			client.fetch();
 		});
 
-		it('should find a the image based on the og:image tag if defined', function(done){
+		it('should find a the image based on the og:image tag if defined', function(done) {
 			client = new MetaInspector("http://www.simple.com", {});
 
-			client.once("fetch", function(){
+			client.once("fetch", function() {
 				client.image.should.equal("http://placehold.it/350x150");
+
 				done();
 			});
 
 			client.fetch();
 		});
 
-		it.skip('should return an array of absolute image paths for all images on the page', function(done){
+		it.skip('should return an array of absolute image paths for all images on the page', function(done) {
 			client = new MetaInspector("http://www.simple.com", {});
 
-			client.once("fetch", function(){
+			client.once("fetch", function() {
 				client.images.should.be.instanceof(Array).and.be.eql(
 					[ 'http://www.simple.com/clouds.jpg',
 						'http://www.simple.com/image/relative.gif',
@@ -247,16 +259,17 @@ describe('metainspector', function(){
 						'http://placehold.it/350x150',
 						'https://placehold.it/350x65',
 						'//placehold.it/350x65' ]);
+
 				done();
 			});
 
 			client.fetch();
 		});
 
-		it('should return an array of rss or atom feeds if defined', function(done){
+		it('should return an array of rss or atom feeds if defined', function(done) {
 			client = new MetaInspector("http://www.simple.com", {});
 
-			client.once("fetch", function(){
+			client.once("fetch", function() {
 				client.feeds.length.should.equal(2);
 				done();
 			});
@@ -264,10 +277,10 @@ describe('metainspector', function(){
 			client.fetch();
 		});
 
-		it('should return the open graph title if defined', function(done){
+		it('should return the open graph title if defined', function(done) {
 			client = new MetaInspector("http://www.simple.com", {});
 
-			client.once("fetch", function(){
+			client.once("fetch", function() {
 				client.ogTitle.should.equal("I am an Open Graph title");
 				done();
 			});
@@ -275,47 +288,51 @@ describe('metainspector', function(){
 			client.fetch();
 		});
 
-		it('should emit errors', function(done){
+		it('should emit errors', function(done) {
 			client = new MetaInspector("http://www.google-404.com/", {});
 
-			client.once("error", function(error){
-				should.exists(error);
+			client.once("error", function(error) {
+				should.exist(error);
+
 				done();
 			});
 
 			client.fetch();
 		});
 
-		it("should return the open graph type, if defined", function(done){
+		it("should return the open graph type, if defined", function(done) {
 			client = new MetaInspector("http://www.techsuplex.com", {});
 
 			client.once("fetch", function() {
-				client.ogType.should.exist;
+				should.exist(client.ogType);
 				client.ogType.should.equal("article");
+
 				done();
 			});
 
 			client.fetch();
 		});
 
-		it('should return the last updated time, if defined', function(done){
+		it('should return the last updated time, if defined', function(done) {
 			client = new MetaInspector("http://www.techsuplex.com", {});
 
 			client.once("fetch", function() {
-				client.ogUpdatedTime.should.exist;
+				should.exist(client.ogUpdatedTime);
 				client.ogUpdatedTime.should.equal("2013-10-31T09:29:46+00:00");
+
 				done();
 			});
 
 			client.fetch();
 		});
 
-		it('should return the open graph locale, if defined', function(done){
+		it('should return the open graph locale, if defined', function(done) {
 			client = new MetaInspector("http://www.techsuplex.com", {});
 
 			client.once("fetch", function() {
-				client.ogLocale.should.exist;
+				should.exist(client.ogLocale);
 				client.ogLocale.should.equal("en_US");
+
 				done();
 			});
 
